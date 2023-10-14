8995b053759ddbc0165b4d79da2c40b0baabb300
V-Yehor <es.valkoff@gmail.com>
2023-10-14 19:12:36 +0300

    First_commit_categories_markup

---
+++ src/js/selectedCategories.js
@@ -0,0 +1,36 @@
+import { getCategoriesList, getBooksByCategory } from './API';
+
+const bookCategory = document.querySelector('.category-tittle');
+const booksList = document.querySelector('.book-list');
+// const list-item = document.querySelector('.categories-item');
+
+// list-item.addEventListener('click', renderBooksMarkup);
+
+// function renderBooksMarkup(event) {
+//   event.preventDefault();
+// }
+
+let category = 'Hardcover Fiction';
+bookCategory.textContent = category;
+
+const data = await getBooksByCategory(category);
+renderBooksMarkup(data);
+
+function renderBooksMarkup(choosenCategory) {
+  const murkup = choosenCategory
+    .map(
+      ({ book_image, title, author }) => `
+      <li class="book-list-item">
+        <img
+          class="book-item-img"
+          src="${book_image}"
+          alt="${title}"
+        />
+        <h3 class="book-item-tittle">${title}</h3>
+        <p class="book-item-text">${author}</p>
+      </li>
+    `
+    )
+    .join('');
+  booksList.innerHTML = murkup;
+}
