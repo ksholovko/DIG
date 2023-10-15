1168590857749b5c05f1db0ff01392caa99e543f
ksholovko <132094880+ksholovko@users.noreply.github.com>
2023-10-15 22:50:58 +0300

    support_Ukraine

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import './js/header';
 import './js/selectedCategories';
 import './js/categories';
-import './js/top-books'
+import './js/top-books';
+import './js/support-ukraine';
 // import './js/modal-window';
